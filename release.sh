319bd816c1969f94d1d2a0724719d9b9557ae715
mraible <mraible@2aeb74e6-0f1c-0410-991c-92d95bb9df37>
2006-10-22 23:24:29 +0000

    Force delete - needed for Subversion
    git-svn-id: https://appfuse.dev.java.net/svn/appfuse/branches/BRANCH_1-9-x@1940 2aeb74e6-0f1c-0410-991c-92d95bb9df37

--- release.sh
+++ release.sh
@@ -7,38 +7,38 @@ rm -rf appfuse-spring-dist
 mkdir appfuse-spring-dist
 cp -r appfuse appfuse-spring-dist/.
 cd appfuse-spring-dist/appfuse
-rm -r extras/myjavapack extras/jsf extras/tapestry extras/webwork
-cd extras/spring;ant install;cd ../..;rm -r extras/spring
+rm -rf extras/myjavapack extras/jsf extras/tapestry extras/webwork
+cd extras/spring;ant install;cd ../..;rm -rf extras/spring
 ant dist -Darchive.prefix=appfuse-springmvc -Dnodocs=true
 cp ../*.zip ../../appfuse/dist/release
 
 cd ../..
 rm -rf appfuse-webwork-dist
 mkdir appfuse-webwork-dist
-cp -r appfuse appfuse-webwork-dist/.
+cp -rf appfuse appfuse-webwork-dist/.
 cd appfuse-webwork-dist/appfuse
-rm -r extras/myjavapack extras/jsf extras/spring extras/tapestry
-cd extras/webwork;ant install;cd ../..;rm -r extras/webwork
+rm -rf extras/myjavapack extras/jsf extras/spring extras/tapestry
+cd extras/webwork;ant install;cd ../..;rm -rf extras/webwork
 ant dist -Darchive.prefix=appfuse-webwork -Dnodocs=true
 cp ../*.zip ../../appfuse/dist/release
 
 cd ../..
 rm -rf appfuse-jsf-dist
 mkdir appfuse-jsf-dist
-cp -r appfuse appfuse-jsf-dist/.
+cp -rf appfuse appfuse-jsf-dist/.
 cd appfuse-jsf-dist/appfuse
-rm -r extras/myjavapack extras/spring extras/tapestry extras/webwork
-cd extras/jsf;ant install;cd ../..;rm -r extras/jsf
+rm -rf extras/myjavapack extras/spring extras/tapestry extras/webwork
+cd extras/jsf;ant install;cd ../..;rm -rf extras/jsf
 ant dist -Darchive.prefix=appfuse-jsf -Dnodocs=true
 cp ../*.zip ../../appfuse/dist/release
 
 cd ../..
 rm -rf appfuse-tapestry-dist
 mkdir appfuse-tapestry-dist
-cp -r appfuse appfuse-tapestry-dist/.
+cp -rf appfuse appfuse-tapestry-dist/.
 cd appfuse-tapestry-dist/appfuse
-rm -r extras/myjavapack extras/jsf extras/spring extras/webwork
-cd extras/tapestry;ant install;cd ../..;rm -r extras/tapestry
+rm -rf extras/myjavapack extras/jsf extras/spring extras/webwork
+cd extras/tapestry;ant install;cd ../..;rm -rf extras/tapestry
 ant dist -Darchive.prefix=appfuse-tapestry -Dnodocs=true
 cp ../*.zip ../../appfuse/dist/release
 
